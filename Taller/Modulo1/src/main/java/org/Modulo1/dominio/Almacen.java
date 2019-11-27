d60b8325b27b63d64b90020536f0c6c3e1a36422
JuanManuelPalaciosNavas <juanma.palacios@hotmail.es>
2019-11-27 01:31:20 +0100

    v1.1.2 - Implementada una clase objeto Almacén que supone dependencias entre las iteraciones 2, 3 y 4.

---
+++ Taller/Modulo1/src/main/java/org/Modulo1/dominio/Almacen.java
@@ -0,0 +1,23 @@
+package Iteracion1.Dominio;
+
+public class Almacen {
+
+	private int id;
+	private String listaPiezas;
+	private String listaPiezasUtilizadas;
+
+	public String cogerPieza() {
+		// TODO - implement Almacen.cogerPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param String
+	 */
+	public void pedirPieza(int String) {
+		// TODO - implement Almacen.pedirPieza
+		throw new UnsupportedOperationException();
+	}
+
+}
