587228d1c0af68dfac9fca7de8bbf3a833625a42
mauro <mauro@PPC-MAURO.home>
2019-11-27 20:10:58 +0100

    v1.3.0 implementada iteración 3

---
+++ Taller/Modulo3/src/main/java/org/Modulo3/Almacen.java
@@ -0,0 +1,33 @@
+package Iteracion3.Dominio;
+
+public class Almacen {
+
+	private int id;
+	private String listaPiezas;
+	private String listaPiezasUtilizadas;
+
+	public String cogerPieza() {
+		// TODO - implement Almacen.cogerPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nuevaPieza
+	 */
+	public void pedirPieza(String nuevaPieza) {
+		// TODO - implement Almacen.pedirPieza
+		throw new UnsupportedOperationException();
+	}
+
+	public ArrayList<String> previsionPiezas() {
+		// TODO - implement Almacen.previsionPiezas
+		return this.listaPiezas;
+	}
+
+	public void bajadaUmbral() {
+		// TODO - implement Almacen.bajadaUmbral
+		throw new UnsupportedOperationException();
+	}
+
+}
