dab40119d2510c1305ecbfc4465a7aea4bda9af7
Christopher29 <Christopher29@Administrador>
2019-11-27 20:48:42 +0100

    v1.2.0 - Implementada la iteración 2.

---
+++ Taller/Modulo2/src/main/java/org/Modulo2/dominio/Almacen.java
@@ -0,0 +1,23 @@
+package Iteracion2.Dominio;
+
+public class Almacen {
+
+	private int id;
+	private String listaPiezas;
+	private String listaPiezasUtilizadas;
+
+	public String cogerPieza() {
+		// TODO - implement Almacen.cogerPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param String
+	 */
+	public void pedirPieza(String nuevaPieza) {
+		// TODO - implement Almacen.nuevaPieza
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Taller/Modulo2/src/main/java/org/Modulo2/dominio/GestorMecanico.java
@@ -0,0 +1,44 @@
+package Iteracion2.Dominio;
+
+public class GestorMecanico {
+
+		public void HacerDiagnostico() {
+			// TODO - implement GestorMecanico.HacerDiagnostico
+			throw new UnsupportedOperationException();
+		}
+
+		public void GestionarEstados() {
+			// TODO - implement GestorMecanico.GestionarEstados
+			throw new UnsupportedOperationException();
+		}
+
+		public void AvisarClientes() {
+			// TODO - implement GestorMecanico.AvisarClientes
+			throw new UnsupportedOperationException();
+		}
+		
+		public void GenerarListaPiezas() {
+			// TODO - implement GestorMecanico.GenerarListaPiezas
+			throw new UnsupportedOperationException();
+		}
+
+		public void ValidarReparacion() {
+			// TODO - implement GestorMecanico.ValidarReparacion
+			throw new UnsupportedOperationException();
+		}
+		
+		public void ProcederReparcaion() {
+			// TODO - implement GestorMecanico.ProcederReparcaion
+			throw new UnsupportedOperationException();
+		}
+		
+		public void CambiarEstadoBox() {
+			// TODO - implement GestorMecanico.CambiarEstadoBox
+			throw new UnsupportedOperationException();
+		}
+		
+		public void EvaluarAvería() {
+			// TODO - implement GestorMecanico.EvaluarAvería
+			throw new UnsupportedOperationException();
+		}
+	}
